b1b2cf8ddf5b9fc1b969d19dca1ee71898fb7d00
Dmitry <dimonshestak160201@gmail.com>
2023-12-20 18:59:35 +0300

    Change buildspec

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# Инициализация NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Использование определенной версии Node.js
+nvm use 19.7.0
+
 mkdir /home/ubuntu/ea-test-application
 cd /home/ubuntu/ea-test-application
 
+# Ваши команды
 sudo npm install
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,11 @@
 cd /home/ubuntu/ea-test-application
 
-node -v
-nvm list
-ls -l
+# Инициализация NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Использование определенной версии Node.js
+nvm use 19.7.0
 
 sudo npm start
