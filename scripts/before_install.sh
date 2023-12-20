ef3334634525f739755a9cf96c346546dbfdc8a4
Dmitry <dimonshestak160201@gmail.com>
2023-12-20 17:51:54 +0300

    Change buildspec

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,5 +5,6 @@ sudo apt-get update
 sudo apt install curl
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 source ~/.bashrc
+ls -la
 nvm install 19.7.0
 nvm use 19.7.0
