5542c0e0e28fea9a39ec4b8e0192215b26ceb226
Dmitry <dimonshestak160201@gmail.com>
2023-12-20 17:57:06 +0300

    Change buildspec

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,8 +3,7 @@
 # Install node.js
 sudo apt-get update
 sudo apt install curl
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-source ~/.bashrc
-ls -la
-nvm install 19.7.0
-nvm use 19.7.0
+sudo curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+sudo source ~/.bashrc
+sudo nvm install 19.7.0
+sudo nvm use 19.7.0
